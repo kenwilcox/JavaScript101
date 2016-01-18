048ec241750926bda1193ce6700602a81118cae4
Ken Wilcox <wilcoxk@gmail.com>
2016-01-18 12:46:31 -0700

    Added Examples

---
+++ examples.js
@@ -0,0 +1,34 @@
+// example 1
+var price = 28.99; 
+var discount = 10; 
+var total = price - (price * (discount / 100)); 
+if (total > 25) {
+  freeShipping(); 
+}
+
+// example 2
+var count = 10; 
+while (count > 0) {
+  juggle();
+  count = count - 1; 
+}
+
+// example 3
+var dog = {name: "Rover", weight: 35};
+if (dog.weight > 30) { 
+  alert("WOOF WOOF");
+} else {
+  alert("woof woof");
+}
+
+// example 4
+var circleRadius = 20; 
+var circleArea = Math.PI * (circleRadius * circleRadius);
+
+
+function freeShipping() {
+  // Do nothing
+}
+function juggle() {
+  // Do nothing
+}
