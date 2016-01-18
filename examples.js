0d46c3ce7220192f72885c4c4921f8459b8159e6
Ken Wilcox <wilcoxk@gmail.com>
2016-01-18 15:32:27 -0700

    added more examples

--- examples.js
+++ examples.js
@@ -45,10 +45,63 @@ while (scoops > 0) {
   scoops = scoops - 1;
 }
 
+var cashInWallet = 10;
+var order;
+
+// if example
+
+if (cashInWallet > 5) {
+  order = "I’ll take the works: cheeseburger, fries and a coke";
+} else {
+  order = "I’ll just have a glass of water"; 
+}
+
 
 function freeShipping() {
   // Do nothing
 }
 function juggle() {
   // Do nothing
+}
+
+
+// 
+if (scoops < 3) {
+  alert("Ice cream is running low!"); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+if (scoops >= 5) {
+  alert("Eat faster, the ice cream is going to melt!");
+} else if (scoops < 3) {
+  alert("Ice cream is running low!"); 
+}
+
+
+
+
+
+
+if (scoops >= 5) {
+  alert("Eat faster, the ice cream is going to melt!");
+} else if (scoops == 3) {
+  alert("Ice cream is running low!");
+} else if (scoops == 2) { 
+  alert("Going once!");
+} else if (scoops == 1) {
+  alert("Going twice!");
+} else if (scoops == 0) {
+  alert("Gone!"); 
+} else {
+  alert("Still lots of ice cream left, come and get it."); 
 }
