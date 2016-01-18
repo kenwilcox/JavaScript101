0297521adb8faf93665c5a41944ec6d1f4cccf22
Ken Wilcox <wilcoxk@gmail.com>
2016-01-18 13:59:24 -0700

    added more examples

--- examples.js
+++ examples.js
@@ -25,6 +25,26 @@ if (dog.weight > 30) {
 var circleRadius = 20; 
 var circleArea = Math.PI * (circleRadius * circleRadius);
 
+// Comments
+// You can start them at the beginning of a line
+var age = 10; // or at the end
+/* Or you can comment out a block of code
+var topSecret = 100;
+var magicNumber = 42;
+*/
+/* or you can just use it to
+write a large note to yourself
+or whoever needs to maintain
+what you created later
+*/
+
+var scoops = 10;
+
+while (scoops > 0) {
+  document.write("Another scoop!"); 
+  scoops = scoops - 1;
+}
+
 
 function freeShipping() {
   // Do nothing
